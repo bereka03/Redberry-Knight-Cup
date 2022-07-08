550f8b566deda928b83b32e8f2a46a786912b081
Giorgi <giorgi.berekashvili.1@btu.edu.ge>
2022-07-09 03:53:32 +0400

    validation

---
+++ js/register.js
@@ -0,0 +1,102 @@
+flatpickr("#date", {
+    
+});
+jQuery.validator.setDefaults({
+    debug: true,
+    success: "valid"
+  });
+$(function() {
+    let $registerForm = $("#registration");
+    if ($registerForm.length){
+        $registerForm.validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                email: {
+                    required: true,
+                    // redberryEmail: true
+                },
+                phone_number: {
+                    required: true,
+                    number: true,
+                    minlength: 9,
+                    maxlength: 9
+                },
+                date: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please Enter your name",
+                    minlength: "Please enter minimum 2 symbol"
+                },
+                email: {
+                    required: "Please Enter your email address",
+                    redberryEmail: "Please Enter valid email"
+                },
+                phone_number: {
+                    required: "Please Enter your phone number",
+                    number: "Please enter only numbers",
+                    minlength: "Please Enter 9 digit",
+                    maxlength: "Please Enter 9 digit"
+                    
+                },
+                date: {
+                    required: "Please Enter your date of birth",
+                    date: true
+                }
+            },
+            errorPlacement: function(error, element){
+                error.insertAfter(element.parent());
+            }
+        });
+        $('.submit').click(function() {
+            if ($registerForm.valid()){
+                location.href = "../index.html";
+            }
+        });
+        let nameInput = $('.name');
+        nameInput.change(function(){
+            if (nameInput.valid()==true && $('.check').hasClass('valid')==false){
+                $('.check').first().addClass('visible');
+            }
+            else {
+                $('.check').first().removeClass('visible');
+            }
+        });
+        let emailInput = $('#email');
+        emailInput.change(function(){
+            if (emailInput.valid()==true){
+                $("#second" ).addClass('visible');
+            }
+            else {
+                $('#second').removeClass('visible');
+            }
+        });
+        let phoneInput = $('#phone');
+        phoneInput.change(function(){
+            if(phoneInput.valid){
+                $('#third').addClass('visible');
+            }
+            else {
+                $('#third').removeClass('visible');
+            }
+        });
+        let dateInput = $('#date');
+        dateInput.change(function(){
+            if(dateInput.valid){
+                $('.check').last().addClass('visible');
+            }
+            else {
+                $('.check').last().removeClass('visible');
+            }
+        });
+    }
+    $.validator.addMethod("redberryEmail", function(value, element){
+        return this.optional(element) || /^.+@redberry.com$/.test(value);
+    }, "Only redberry email adresses are allowed");
+})
+

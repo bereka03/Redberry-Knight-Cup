68f1b481056bed080a544a7a5d0cc3aa1da2ab08
Giorgi <giorgi.berekashvili.1@btu.edu.ge>
2022-07-09 16:59:19 +0400

    add local storage

--- js/register.js
+++ js/register.js
@@ -1,12 +1,13 @@
 flatpickr("#date", {
-    
+    dateFormat: 'd/m/Y'
 });
+// validaition
 jQuery.validator.setDefaults({
     debug: true,
     success: "valid"
   });
+let $registerForm = $("#registration");
 $(function() {
-    let $registerForm = $("#registration");
     if ($registerForm.length){
         $registerForm.validate({
             rules: {
@@ -16,7 +17,7 @@ $(function() {
                 },
                 email: {
                     required: true,
-                    // redberryEmail: true
+                    redberryEmail: true
                 },
                 phone_number: {
                     required: true,
@@ -55,7 +56,13 @@ $(function() {
         });
         $('.submit').click(function() {
             if ($registerForm.valid()){
-                location.href = "../index.html";
+                location.href = "../pages/experience.html";
+            }
+        });
+        $('input').change(function(){
+            if ($registerForm.valid()){
+                $('#pers').html("<img src='../img/box-check.svg' alt='check-icon'>");
+                $('.check').addClass('visible');
             }
         });
         let nameInput = $('.name');
@@ -98,5 +105,27 @@ $(function() {
     $.validator.addMethod("redberryEmail", function(value, element){
         return this.optional(element) || /^.+@redberry.com$/.test(value);
     }, "Only redberry email adresses are allowed");
-})
+    
+});
+// localstorage
+window.onbeforeunload = function() {
+    localStorage.setItem("name", $('#name').val());
+    localStorage.setItem("email", $('#email').val());
+    localStorage.setItem("phone", $('#phone').val());
+    localStorage.setItem("date", $('#date').val());    
+}
+window.onload = function() {
+    let name = localStorage.getItem("name");
+    if (name !== null) $('#name').val(name);
+   
+    let email = localStorage.getItem("email");
+    if (email !== null) $('#email').val(email);
+
+    let phone = localStorage.getItem("phone");
+    if (phone !== null) $('#phone').val(phone);
+
+    let date = localStorage.getItem("date");
+    if (date !== null) $('#date').val(date);
+
+}
 
